3af6bf526d1ddb5f4cc88dd1062405858119b666
sunkuan <sunkuan@wondersgroup.com>
2015-10-26 21:52:25 +0800

    Add enhanced `DateData.java`

--- mcalendarview/src/main/java/sun/bob/mcalendarview/vo/DateData.java
+++ mcalendarview/src/main/java/sun/bob/mcalendarview/vo/DateData.java
@@ -7,11 +7,15 @@ public class DateData {
     private int year;
     private int month;
     private int day;
+    private int hour;
+    private int minute;
 
     public DateData(int year, int month, int day){
         this.day = day;
         this.month = month;
         this.year = year;
+        this.hour = 0;
+        this.minute = 0;
     }
 
     public int getYear() {
@@ -36,6 +40,30 @@ public class DateData {
 
     public void setDay(int day) {
         this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public String getHourString(){
+        return hour > 10 ? String.format("%d", hour) : String.format("0%d", hour);
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public String getMinuteString(){
+        return minute > 10 ? String.format("%d", minute) : String.format("0%d", minute);
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
     }
 
     @Override
